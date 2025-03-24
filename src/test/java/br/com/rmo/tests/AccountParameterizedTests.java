73ff0a9c4883726eb7337733ba8d3fd0591098f8
ranimoura <raniereoliveira@gmail.com>
2025-03-24 17:47:19 -0300

    ajustes nos imports e alterações no método ClearBalance da classe ParameterizedTest

--- src/test/java/br/com/rmo/tests/AccountParameterizedTests.java
+++ src/test/java/br/com/rmo/tests/AccountParameterizedTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.Timeout;
